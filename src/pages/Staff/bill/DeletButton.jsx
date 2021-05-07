414e4e4be416b0128add0ab3afc48801e05228f9
anoopkjacob <9745360897akj@gmail.com>
2021-05-07 15:37:26 +0530

    carthalf

--- src/pages/Staff/bill/DeletButton.jsx
+++ src/pages/Staff/bill/DeletButton.jsx
@@ -1,70 +1,69 @@
-import React from 'react';
+import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { FaTimes } from "react-icons/fa";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
 import axios from "axios";
 
+toast.configure();
 
-toast.configure()
+export default function DeleteButton({ Rows }) {
+  // const userid =localStorage.getItem('loginid');
+  const billid = Rows.billid;
+  console.log( Rows.productId.productid);
+  console.log( Rows.noofitems);
 
-
-export default function DeleteButton({Rows}) {
-
-
-
-// const userid =localStorage.getItem('loginid');
-const billid= Rows.billid;
-
-
-
-    const handlepage=()=>{
-        try {
-            axios
-              .post(`http://localhost:5000/bill/billitemDelete`,{billid})
-              .then((resp) => {
-                console.log(resp);
-                if(resp.data.message==="deleted") {
-                  toast.success(`${resp.data.message}`,{
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: true,
-                    progress: undefined});
-                    setTimeout(() => {
-                      window.location.reload(false)
-                    }, 3000);
-                }else{
-                  toast.error(`${resp.data.message}`,{
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: true,
-                    progress: undefined});
-                }
-              });
-          } catch (e) {
-            console.log(e.data);
+  const handlepage = () => {
+    try {
+      axios
+        .post(`http://localhost:5000/bill/billitemDelete`, {
+          billid,
+          productid: Rows.productId.productid,
+          quantity: Rows.noofitems,
+        })
+        .then((resp) => {
+          console.log(resp);
+          if (resp.data.message === "deleted") {
+            toast.success(`${resp.data.message}`, {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: true,
+              progress: undefined,
+            });
+            setTimeout(() => {
+              window.location.reload(false);
+            }, 3000);
+          } else {
+            toast.error(`${resp.data.message}`, {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: true,
+              progress: undefined,
+            });
           }
+        });
+    } catch (e) {
+      console.log(e.data);
     }
-   
+  };
 
-     
-
-    return (
-        <div>
-
-        <Button variant="danger" type="submit" className="m-2 "  onClick={()=>handlepage()}  >
-           <FaTimes/>
-        </Button>
-  
-        </div>
-    )
+  return (
+    <div>
+      <Button
+        variant="danger"
+        type="submit"
+        className="m-2 "
+        onClick={() => handlepage()}
+      >
+        <FaTimes />
+      </Button>
+    </div>
+  );
 }
-
-
--- src/pages/customer/cart/cartcards.jsx
+++ src/pages/customer/cart/cartcards.jsx
@@ -1,53 +1,165 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Card,Button } from "react-bootstrap";
-import { FaPlus,FaMinus } from "react-icons/fa";
+import { Card, Button } from "react-bootstrap";
+import { FaPlus, FaMinus } from "react-icons/fa";
 import { GrClose } from "react-icons/gr";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
 import axios from "axios";
 
 export default function Cartcards({ data }) {
-
-    const minusaction=()=>{
-     console.log("minus")
-    };
-    const plusaction=()=>{
-        console.log("plus") 
-    };
-
-  const cartdelete =(_id)=>{
+  const minusaction = () => {
+    if(data.quantity===1)  
+      {
+        toast.error("atleast 1", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      } else{
     try {
       axios
-        .post(`http://localhost:5000/cart/delete`,{_id})
+        .post(`http://localhost:5000/cart/minus`, {
+          unitprice: data.productid.unitprice,
+          productid: data.productid.productid,
+          quantity:data.quantity,
+          _id:data._id,
+        })
         .then((resp) => {
           console.log(resp);
-          if(resp.data.message==="deleted") {
-            toast.success(`${resp.data.message}`,{
+
+          if (resp.data.message === "cart updated" ) {
+            toast.success(`${resp.data.message}`, {
               position: "bottom-right",
               autoClose: 5000,
               hideProgressBar: true,
               closeOnClick: true,
               pauseOnHover: false,
               draggable: true,
-              progress: undefined});
-              setTimeout(() => {
-                window.location.reload(false)
-              }, 3000);
-          }else{
-            toast.error(`${resp.data.message}`,{
+              progress: undefined,
+            });
+            setTimeout(() => {
+              window.location.reload(false);
+            }, 2000);
+          } else {
+            toast.error(`${resp.data.message}`, {
               position: "bottom-right",
               autoClose: 5000,
               hideProgressBar: true,
               closeOnClick: true,
               pauseOnHover: false,
               draggable: true,
-              progress: undefined});
+              progress: undefined,
+            });
           }
         });
     } catch (e) {
       console.log(e.data);
     }
-  }  
+  }
+  };
+  const plusaction = () => {
+      if(data.productid.quantity===0)  
+      {
+        toast.error("stocke is less", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      }else if(data.quantity===4){
+        toast.error("your limit has exceed", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+       else{
+    try {
+      axios
+        .post(`http://localhost:5000/cart/plus`, {
+          unitprice: data.productid.unitprice,
+          productid: data.productid.productid,
+          quantity:data.quantity,
+          _id:data._id,
+        })
+        .then((resp) => {
+          console.log(resp);
+
+          if (resp.data.message === "cart updated") {
+            toast.success(`${resp.data.message}`, {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: true,
+              progress: undefined,
+            });
+            setTimeout(() => {
+              window.location.reload(false);
+            }, 2000);
+          } else {
+            toast.error(`${resp.data.message}`, {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: true,
+              progress: undefined,
+            });
+          }
+        });
+    } catch (e) {
+      console.log(e.data);
+    }
+  }
+  };
+
+  const cartdelete = (_id) => {
+    try {
+      axios.post(`http://localhost:5000/cart/delete`, { _id,quantity:data.quantity,productid:data.productid.productid}).then((resp) => {
+        console.log(resp);
+        if (resp.data.message === "deleted") {
+          toast.success(`${resp.data.message}`, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+          setTimeout(() => {
+            window.location.reload(false);
+          }, 3000);
+        } else {
+          toast.error(`${resp.data.message}`, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      });
+    } catch (e) {
+      console.log(e.data);
+    }
+  };
 
   return (
     <Card
@@ -55,20 +167,23 @@ export default function Cartcards({ data }) {
       text="dark"
       className="text-center p-4 m-4"
     >
-        <Card.Header style={{ display: "flex",justifyContent:"flex-end"}}>
-          <Button variant="danger" onClick={()=>cartdelete(data._id)}><GrClose/></Button>
-       </Card.Header>
+      <Card.Header style={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button variant="danger" onClick={() => cartdelete(data._id)}>
+          <GrClose />
+        </Button>
+      </Card.Header>
       <Card.Body className="p-2">
         <Card.Title className="p-1">{data.productid.productname}</Card.Title>
         <Card.Text className="text-center">
-      
           <strong>
-            size:{data.productid.size}{data.productid.units} || Color:
-            {data.productid.color} || Price:{data.productid.unitprice}{" || "}
+            size:{data.productid.size}
+            {data.productid.units} || Color:
+            {data.productid.color} || Price:{data.productid.unitprice}
+            {" || "}
           </strong>
-       
+
           <span>
-              <strong>Quantity</strong>
+            <strong>Quantity</strong>
             <Button
               variant="danger"
               type="submit"
@@ -86,8 +201,8 @@ export default function Cartcards({ data }) {
             >
               <FaPlus />
             </Button>
-           <strong>|| price: </strong> 
-           <input type="text" value={data.totalprice} readOnly />
+            <strong>|| price: </strong>
+            <input type="text" value={data.totalprice} readOnly />
           </span>
         </Card.Text>
       </Card.Body>
